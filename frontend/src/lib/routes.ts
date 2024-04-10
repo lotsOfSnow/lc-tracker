f52a93e21005248a07d08c9ea20e3ceabe2d5b2a
lotsOfSnow <lotsofsnow.dev@gmail.com>
2024-04-10 21:30:05 +0200

    Setup nav

---
+++ frontend/src/lib/routes.ts
@@ -0,0 +1,5 @@
+export const routes = {
+	home: '/',
+	problems: '/problems',
+	attempts: '/attempts'
+};
---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { routes } from '$lib/routes';
+
+	interface Item {
+		text: string,
+		link: string,
+	}
+
+	const items: Item[] = [
+		{
+			text: 'Home',
+			link: routes.home
+		},
+		{
+			text: 'Problems',
+			link: routes.problems
+		},
+		{
+			text: 'Attempts',
+			link: routes.attempts
+		}
+	];
+</script>
+<nav>
+	<ul>
+		{#each items as item}
+			<li><a href="{item.link}">{item.text}</a></li>
+		{/each}
+	</ul>
+</nav>
+
+<slot />
