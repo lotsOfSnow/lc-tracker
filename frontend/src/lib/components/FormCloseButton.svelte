fb8d89fbaa0e0ca753f8a09e3012ee5e82419b72
lotsOfSnow <lotsofsnow.dev@gmail.com>
2024-04-13 01:40:55 +0200

    Make form nicer

---
+++ frontend/src/lib/components/FormCloseButton.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  export let to: string;
+</script>
+
+<a href={to} class="close hover:scale-125 transition duration-100 ease-in-out" tabindex="0"
+   role="button">close</a>
+
+<style>
+    .close {
+        position: absolute;
+        top: 0;
+        right: 0;
+        display: block;
+        width: 40px;
+        height: 40px;
+        font-size: 0;
+    }
+
+    .close:before,
+    .close:after {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        width: 5px;
+        height: 20px;
+        background-color: black;
+        transform: rotate(45deg) translate(-50%, -50%);
+        transform-origin: top left;
+        content: '';
+    }
+
+    .close:after {
+        transform: rotate(-45deg) translate(-50%, -50%);
+    }
+</style>
--- frontend/src/routes/problems/new/+page.svelte
+++ frontend/src/routes/problems/new/+page.svelte
@@ -3,35 +3,40 @@
   import Input from '$lib/components/Input.svelte';
   import Label from '$lib/components/Label.svelte';
   import Button from '$lib/components/Button.svelte';
+  import FormCloseButton from '$lib/components/FormCloseButton.svelte';
+  import { AppRoute } from '$lib/routes';
 
   export let form;
 </script>
 
-<form method="POST" on:submit|preventDefault use:enhance>
-  <div>
-    <Label for="number">Number</Label>
-    <Input id="number" name="number" type="number" />
-  </div>
+<div class="bg-gray-100 p-6 w-full max-w-md mx-auto rounded-lg shadow-md relative">
+  <FormCloseButton to={AppRoute.PROBLEMS} />
+  <form method="POST" on:submit|preventDefault use:enhance>
+    <div>
+      <Label for="number">Number</Label>
+      <Input id="number" name="number" type="number" />
+    </div>
 
-  <div>
-    <Label for="name">Name</Label>
-    <Input id="name" name="name" type="text" />
-  </div>
+    <div>
+      <Label for="name">Name</Label>
+      <Input id="name" name="name" type="text" />
+    </div>
 
-  <div>
-    <Label for="url">Url</Label>
-    <Input id="url" name="url" type="text" />
-  </div>
+    <div>
+      <Label for="url">Url</Label>
+      <Input id="url" name="url" type="text" />
+    </div>
 
-  <Button type="submit">Create</Button>
-</form>
+    <Button type="submit">Create</Button>
+  </form>
 
-{#if form?.fieldErrors || form?.formErrors}
-  <div>
-    <ul>
-      {#each ([...Object.entries(form.fieldErrors), ...Object.entries(form.formErrors)]).filter(x => x) as error}
-        <li class="mt-2 text-sm text-red-500">{error}</li>
-      {/each}
-    </ul>
-  </div>
-{/if}
+  {#if form?.fieldErrors || form?.formErrors}
+    <div>
+      <ul>
+        {#each ([...Object.entries(form.fieldErrors), ...Object.entries(form.formErrors)]).filter(x => x) as error}
+          <li class="mt-2 text-sm text-red-500">{error}</li>
+        {/each}
+      </ul>
+    </div>
+  {/if}
+</div>
