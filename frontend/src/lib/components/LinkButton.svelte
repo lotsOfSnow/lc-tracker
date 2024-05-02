860691cabda7f67d50c7a4273f9cc632908b7bad
lotsOfSnow <lotsofsnow.dev@gmail.com>
2024-05-02 17:31:56 +0200

    Frontend

--- frontend/src/lib/components/LinkButton.svelte
+++ frontend/src/lib/components/LinkButton.svelte
@@ -4,9 +4,11 @@
   let className: ClassName = undefined;
   export { className as class };
   export let href: string;
+
+  $: console.log(href);
 </script>
 
-<a {...$$restProps} href={href}
+<a {...$$restProps} href={href} on:click
    class={cn("inline-block bg-gray-700 hover:bg-gray-600 text-white font-medium py-2 px-4 rounded-lg transition duration-300 ease-in-out", className)}>
   <slot />
 </a>
