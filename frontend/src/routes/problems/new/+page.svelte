442996bb9b59368a14130bc46b3aa2aab6628ffe
lotsOfSnow <lotsofsnow.dev@gmail.com>
2024-05-03 17:42:57 +0200

    Show methods

--- frontend/src/routes/problems/new/+page.svelte
+++ frontend/src/routes/problems/new/+page.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
   import { enhance } from '$app/forms';
   import Input from '$lib/components/Input.svelte';
   import Label from '$lib/components/Label.svelte';
@@ -6,8 +6,21 @@
   import FormCloseButton from '$lib/components/FormCloseButton.svelte';
   import { AppRoute } from '$lib/routes';
   import FormErrors from '$lib/components/form/FormErrors.svelte';
+  import type { apiSchemas } from '$lib/api';
+  import { commonInputControlClassName } from '$lib/components/styleUtils';
 
   export let form;
+
+  let methods: (apiSchemas['ProblemMethod'] & { id: number })[] = [];
+
+  const addMethod = () => {
+    methods = [...methods, { id: methods.length + 1, name: 'aa' }];
+  };
+
+  const removeMethod = (id: number) => {
+    methods = methods.filter(method => method.id !== id);
+  };
+
 </script>
 
 <div class="bg-gray-100 p-6 w-full max-w-md mx-auto rounded-lg shadow-md relative">
@@ -28,6 +41,31 @@
     <div>
       <Label for="url">Url</Label>
       <Input required name="url" id="url" type="url" />
+    </div>
+
+    <div class="mt-4">
+      <Label class="text-2xl">Methods</Label>
+
+      {#each methods as method}
+        <div class="mb-2 border border-gray-300 rounded-md p-2 relative">
+          <button type="button"
+                  class="absolute top-[-8px] right-1 text-4xl"
+                  on:click={() => removeMethod(method.id)}>
+            &times;
+          </button>
+          <div>
+            <Label for={`name-${method.id}`}>Name</Label>
+            <Input required name={`name-${method.id}`} id={`name-${method.id}`} value={method.name} type="text" />
+          </div>
+          <div>
+            <Label for={`contents-${method.id}`}>Content</Label>
+            <textarea id={`contents-${method.id}`} name={`contents-${method.id}`}
+                      class={commonInputControlClassName}>{method.contents || ''}</textarea>
+          </div>
+        </div>
+      {/each}
+
+      <Button type="button" on:click={addMethod}>Add</Button>
     </div>
 
     <Button type="submit" class="mt-2">Create</Button>
