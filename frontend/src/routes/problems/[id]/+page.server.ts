6dc5635ea7cce0a348f255e2d886ebd733e08902
lotsOfSnow <lotsofsnow.dev@gmail.com>
2024-05-16 23:41:46 +0200

    always server

--- frontend/src/routes/problems/[id]/+page.server.ts
+++ frontend/src/routes/problems/[id]/+page.server.ts
@@ -1,9 +1,24 @@
-import { type Actions } from '@sveltejs/kit';
+import { type Actions, error } from '@sveltejs/kit';
 import { z } from 'zod';
 import { apiClient, getApiOperation } from '$lib/api';
 import { safeParseRequestFormData } from '$lib/utils/zodUtils';
 import { problemSchema } from '../common/problemSchema';
 import { failServer, failValidation } from '$lib/utils/actionsReturnTypes';
+
+const operation = getApiOperation('/api/problems/{id}', 'get', 200);
+
+export const load = async ({ params, fetch }) => {
+  const id = params.id;
+
+  const result = await apiClient.GET(operation.path, {
+    fetch,
+    params: {
+      path: { id: id },
+    },
+  });
+
+  return result.data ?? error(400);
+};
 
 export const actions = {
   default: async (event) => {
