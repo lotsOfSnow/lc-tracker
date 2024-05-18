9e64d3f3832bd6483f969583b5c34e35e07bc242
lotsOfSnow <lotsofsnow.dev@gmail.com>
2024-05-18 18:56:32 +0200

    run with secret envs

---
+++ docker/start_prod.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+DEFAULT_ENV_FILE=".env"
+SECRET_ENV_FILE=".env.secret"
+
+DOCKER_COMPOSE_CMD="docker-compose --env-file $DEFAULT_ENV_FILE --env-file $SECRET_ENV_FILE"
+
+$DOCKER_COMPOSE_CMD up
