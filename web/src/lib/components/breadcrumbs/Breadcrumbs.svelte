c69ea138d27fadaf31cd6e9d75dbdc2938025d6f
lotsOfSnow <lotsofsnow.dev@gmail.com>
2024-05-23 22:50:06 +0200

    ui

--- web/src/lib/components/breadcrumbs/Breadcrumbs.svelte
+++ web/src/lib/components/breadcrumbs/Breadcrumbs.svelte
@@ -12,16 +12,22 @@
 </script>
 
 <nav aria-label="breadcrumb">
-  <ul class="flex space-x-2">
+  <ul class="flex items-center space-x-2">
     {#each items as item, index}
       <li class="flex items-center">
         {#if item.route}
-          <a href={item.route} class="text-blue-600 hover:underline">{item.label}</a>
+          <a href={item.route}
+             class="px-3 py-1 bg-gray-300 text-gray-800 font-medium rounded-lg hover:bg-gray-400 transition duration-300">
+            {item.label}
+          </a>
         {:else}
-          <span>{item.label}</span>
+          <span class="px-3 py-1 bg-gray-200 text-gray-600 font-medium rounded-lg">
+            {item.label}
+          </span>
         {/if}
         {#if index < items.length - 1}
-          <svg class="mx-2 h-4 w-4 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
+          <svg class="mx-2 h-5 w-5 text-gray-800" fill="currentColor"
+               viewBox="0 0 20 20">
             <path fill-rule="evenodd"
                   d="M7.707 14.707a1 1 0 010-1.414L11.586 10 7.707 6.707a1 1 0 111.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
                   clip-rule="evenodd" />
