c5761f011d6d7935076f6309f39abb72e2408f55
lotsOfSnow <lotsofsnow.dev@gmail.com>
2024-05-27 21:40:32 +0200

    test

--- server/tests/LcTracker.Api.FunctionalTests/Features/Attempts/UpdateAttemptTests.cs
+++ server/tests/LcTracker.Api.FunctionalTests/Features/Attempts/UpdateAttemptTests.cs
@@ -2,6 +2,8 @@ using LcTracker.Api.FunctionalTests.Bootstrap.TestBase;
 using LcTracker.Api.FunctionalTests.Bootstrap.TestFixture;
 using LcTracker.Core.Features.Attempts;
 using LcTracker.Core.Features.Attempts.Commands;
+using LcTracker.Core.Features.Problems;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LcTracker.Api.FunctionalTests.Features.Attempts;
 
@@ -14,11 +16,7 @@ public class UpdateAttemptTests(ApiTestFixture fixture) : AttemptTest(fixture)
         var now = Context.Time.SetUtcNow(1.January(2024));
         var userId = await Client.RunAsNewUserAsync();
         var attempt = await RequireAttempt(userId);
-        var request = Arrange
-            .Build<UpdateAttemptRequest>()
-            .With(x => x.ProblemId, attempt.ProblemId)
-            .With(x => x.Date, DateOnly.FromDateTime(now.AddDays(-1)))
-            .Create();
+        var request = GetRequest(attempt.ProblemId);
 
         var result = await Client
             .PutAsync<EmptyResponse>($"api/attempts/{attempt.Id}", request);
@@ -26,5 +24,40 @@ public class UpdateAttemptTests(ApiTestFixture fixture) : AttemptTest(fixture)
         result.Response.Should().BeSuccessful();
         var data = await Context.Database.GetAsync<Attempt>(attempt.Id);
         data.Should().BeEquivalentTo(request);
+    }
+
+    [Fact]
+    public async Task Disallows_updating_locked()
+    {
+        var userId = await Client.RunAsNewUserAsync();
+        var problem = await RequireProblem(userId);
+        var original = await Context.Require.Object(
+            Arrange
+                .BuildWithUserId<Attempt>(userId)
+                .With(x => x.ProblemId, problem.Id)
+                .With(x => x.IsLocked, true)
+                .Create());
+        var request = GetRequest(problem.Id);
+
+        var result = await Client.PutAsync<ProblemDetails>($"api/attempts/{original.Id}", request);
+
+        result.Response.Should().HaveError();
+        result.Data.Should().BeEquivalentTo(new
+        {
+            Status = 409, Type = "general/locked",
+        });
+        var existing = await Context.Database.GetAsync<Attempt>(original.Id);
+        existing.Should().BeEquivalentTo(original);
+    }
+
+    private UpdateAttemptRequest GetRequest(Guid problemId)
+    {
+        var now = Context.Time.SetUtcNow(1.January(2024));
+
+        return Arrange
+            .Build<UpdateAttemptRequest>()
+            .With(x => x.ProblemId, problemId)
+            .With(x => x.Date, DateOnly.FromDateTime(now.AddDays(-1)))
+            .Create();
     }
 }
--- server/tests/LcTracker.Api.FunctionalTests/Features/Problems/UpdateProblemTests.cs
+++ server/tests/LcTracker.Api.FunctionalTests/Features/Problems/UpdateProblemTests.cs
@@ -2,6 +2,7 @@ using LcTracker.Api.FunctionalTests.Bootstrap.TestBase;
 using LcTracker.Api.FunctionalTests.Bootstrap.TestFixture;
 using LcTracker.Core.Features.Problems;
 using LcTracker.Core.Features.Problems.Commands;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LcTracker.Api.FunctionalTests.Features.Problems;
 
@@ -20,5 +21,27 @@ public class UpdateProblemTests(ApiTestFixture fixture) : ProblemTest(fixture)
         result.Response.Should().BeSuccessful();
         var updated = await Context.Database.GetAsync<Problem>(problem.Id);
         updated.Should().BeEquivalentTo(request);
+    }
+
+    [Fact]
+    public async Task Disallows_updating_locked()
+    {
+        var userId = await Client.RunAsNewUserAsync();
+        var original = await Context.Require.Object(
+            Arrange
+                .BuildWithUserId<Problem>(userId)
+                .With(x => x.IsLocked, true)
+                .Create());
+        var request = Arrange.Create<UpdateProblemRequest>();
+
+        var result = await Client.PutAsync<ProblemDetails>($"api/problems/{original.Id}", request);
+
+        result.Response.Should().HaveError();
+        result.Data.Should().BeEquivalentTo(new
+        {
+            Status = 409, Type = "general/locked",
+        });
+        var existing = await Context.Database.GetAsync<Problem>(original.Id);
+        existing.Should().BeEquivalentTo(original, x => x.Excluding(p => p.AddedAt));
     }
 }
